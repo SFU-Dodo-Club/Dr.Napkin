aaf28fa11272bc3cb71742f10131759df9825bd8
Sarbjot <sarbjotmann98@gmail.com>
2021-01-12 12:03:08 -0800

    Added a command so Napkin wont send a messsage during inactive hours

--- bot.py
+++ bot.py
@@ -1,21 +1,33 @@
 import discord
 import asyncio
+import datetime 
 import os
 import random
+from google_trans_new import google_translator  
 from discord.ext import commands, tasks
 
-client = commands.Bot(command_prefix = ',')
-client.remove_command('help')
+client = commands.Bot(command_prefix = '-')
 
+translator = google_translator()  
 @client.event
 async def on_ready():
     print("Bot is Ready")
     drinkwater.start()
 
-@tasks.loop(minutes=60)
+@tasks.loop(minutes=90)
 async def drinkwater():
-    channel = client.get_channel(744817323973804093)
-    await channel.send("Make sure to drink water Dodos!")
+    timenow = str(datetime.datetime.now().time())
+    timenow = timenow.split(':')
+    if (int(timenow[0]) < 9 or int(timenow[0]) > 22):
+        pass
+    else:
+        channel = client.get_channel(744817323973804093)
+        await channel.send("Make sure to drink water Dodos!")
+
+@client.command()
+async def translate(ctx,*,sentence):
+    translate_text = translator.translate(sentence,lang_tgt='en')  
+    await ctx.send(translate_text)
 
 
 #Blackbox
