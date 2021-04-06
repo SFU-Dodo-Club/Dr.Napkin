5ee2f55e3a841c7a2532ea532e2a5264fcf64a05
Sarbjotm <sarbjotmann98@gmail.com>
2021-04-06 00:56:46 -0700

    Gave napkin 3 different messages to send

--- bot.py
+++ bot.py
@@ -15,13 +15,15 @@ async def on_ready():
 
 @tasks.loop(minutes=90)
 async def drinkwater():
+    messages = ["Make sure to drink water Dodos!", "Love you dodos! Make sure to stay hydrated", "Guess what time it is: ||Drinking water time||. Love you Dodos!"]
     guild = client.get_guild(744817281871249428)
     channel = guild.get_channel(801326450396758076)
     timenow = str(datetime.datetime.now().time())
     timenow = timenow.split(':')
     print(int(timenow[0]))
     if ((int(timenow[0]) >= 17) or (int(timenow[0]) <= 8)):
-        await channel.send("Make sure to drink water Dodos!")
+        m = random.randint(1,3)
+        await channel.send(messages[m])
 
 # @tasks.loop(minutes = 90)
 # async def goodmorning():
