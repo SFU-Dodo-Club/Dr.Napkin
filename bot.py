eeae5eabb79d09a5e23a318a9ad7c47193164a47
Sarbjot Mann <44513277+Sarbjotm@users.noreply.github.com>
2021-04-25 13:54:33 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,7 +28,7 @@ async def drinkwater():
     print(int(timenow[0]))
     if ((int(timenow[0]) >= 17) or (int(timenow[0]) <= 8)):
         m = random.randint(0,2)
-        await channel.send(f"{messages[m]} \nhttps://media.discordapp.net/attachments/744817323973804093/834601128598700052/image0.png?width=610&height=595")
+        await channel.send(f"{messages[m]} {file = discord.File('water.png')}")
 
 @tasks.loop(minutes=1440)
 async def songOTD():
